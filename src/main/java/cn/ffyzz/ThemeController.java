6ee19c7f3fbe70e23afe4bc74514b9b855a7256d
FFYzz <cryptochen95@gmail.com>
2020-12-09 20:24:11 +0800

    CookieThemeResolver demo

--- src/main/java/cn/ffyzz/ThemeController.java
+++ src/main/java/cn/ffyzz/ThemeController.java
@@ -1,7 +1,15 @@
 package cn.ffyzz;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ThemeResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 /**
  * @Title:
@@ -19,6 +27,21 @@ public class ThemeController {
 
     @GetMapping("/sessionTheme")
     public String sessionTheme() {
+        return "theme";
+    }
+
+    @GetMapping(value = "/cookieTheme", produces = "text/html;charset=UTF-8")
+    public String setCookieTheme(HttpServletRequest request, HttpServletResponse response, String theme) {
+        ThemeResolver themeResolver = RequestContextUtils.getThemeResolver(request);
+        if (StringUtils.isEmpty(theme)) {
+            theme = "default";
+        }
+        themeResolver.setThemeName(request, response, theme);
+        return "redirect:showCookieTheme";
+    }
+
+    @GetMapping("/showCookieTheme")
+    public String cookieTheme() {
         return "theme";
     }
 
